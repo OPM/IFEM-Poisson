9ee516012ae9ab3873668cff6396e30cf6610002
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2012-03-13 14:41:55 +0000

    Moved parsing of expression functions for analytic solutions to two new AnaSol constructors
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1520 e10b68d5-8a6e-419e-a041-bce267b0401d

--- SIMPoisson1D.C
+++ SIMPoisson1D.C
@@ -13,11 +13,9 @@
 
 #include "SIMPoisson1D.h"
 #include "PoissonSolutions.h"
+#include "Functions.h"
 #include "Utilities.h"
 #include "AnaSol.h"
-#include <string.h>
-#include "Functions.h"
-
 #include "tinyxml.h"
 
 
@@ -41,7 +39,7 @@ bool SIMPoisson1D::parse (char* keyWord, std::istream& is)
     {
       int    code  = atoi(strtok(cline," "));
       double kappa = atof(strtok(NULL," "));
-      std::cout <<"\tMaterial code "<< code <<":"<< kappa << std::endl;
+      std::cout <<"\tMaterial code "<< code <<": "<< kappa << std::endl;
       if (code == 0)
 	prob.setMaterial(kappa);
       else if (this->setPropertyType(code,Property::MATERIAL,mVec.size()))
@@ -71,8 +69,8 @@ bool SIMPoisson1D::parse (char* keyWord, std::istream& is)
 
   else if (!strncasecmp(keyWord,"ANASOL",6))
   {
-    cline = strtok(keyWord+6," ");
     int code = -1;
+    cline = strtok(keyWord+6," ");
     if (!strncasecmp(cline,"LINE",4))
     {
       double L = atof(strtok(NULL," "));
@@ -81,40 +79,10 @@ bool SIMPoisson1D::parse (char* keyWord, std::istream& is)
     }
     else if (!strncasecmp(cline,"EXPRESSION",10))
     {
-      std::string primary, secondary, variables;
-      int lines = atoi(strtok(NULL, " "));
-      char* c = strtok(NULL, " ");
-      if (c)
-        code = atoi(c);
-      else
-        code = 0;
-      RealFunc* s=NULL;
-      VecFunc* v=NULL;
-      for (int i = 0; i < lines; i++) {
-        std::string function = utl::readLine(is);
-        size_t pos;
-        if ((pos = function.find("Variables=")) != std::string::npos) {
-          variables += function.substr(pos+10);
-          if (variables[variables.size()-1] != ';')
-            variables += ";";
-        }
-        if ((pos = function.find("Primary=")) != std::string::npos) {
-          primary = function.substr(pos+8);
-          s = new EvalFunction((variables+primary).c_str());
-        }
-        if ((pos = function.find("Secondary=")) != std::string::npos) {
-          secondary = function.substr(pos+10);
-          v = new VecFuncExpr(secondary,variables);
-        }
-      }
-      std::cout <<"\nAnalytical solution:" << std::endl;
-      if (!variables.empty())
-        std::cout << "\t Variables=" << variables << std::endl;
-      if (s)
-        std::cout << "\t Primary=" << primary << std::endl;
-      if (v)
-        std::cout << "\t Secondary=" << secondary << std::endl;
-      mySol = new AnaSol(s, v);
+      std::cout <<"\nAnalytical solution: Expression"<< std::endl;
+      int lines = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
+      code = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
+      mySol = new AnaSol(is,lines);
     }
     else
     {
@@ -144,102 +112,75 @@ bool SIMPoisson1D::parse (char* keyWord, std::istream& is)
 bool SIMPoisson1D::parse (const TiXmlElement* elem)
 {
   if (strcasecmp(elem->Value(),"poisson"))
-    return SIM1D::parse(elem);
+    return this->SIM1D::parse(elem);
 
   std::vector<const TiXmlElement*> parsed = handlePriorityTags(elem);
+
   const TiXmlElement* child = elem->FirstChildElement();
   while (child) {
-    if (find(parsed.begin(),parsed.end(),child) != parsed.end()) {
-      child = child->NextSiblingElement();
-      continue;
-    }
-    if (!strcasecmp(child->Value(),"isotropic")) {
+    if (find(parsed.begin(),parsed.end(),child) != parsed.end())
+      ;
+
+    else if (!strcasecmp(child->Value(),"isotropic")) {
       int code = 0;
-      if (child->Attribute("code"))
-        code = atoi(child->Attribute("code"));
-      double kappa=1000.f;
-      if (child->Attribute("kappa"))
-        kappa = atof(child->Attribute("kappa"));
+      double kappa = 1000.0;
+      utl::getAttribute(child,"code",code);
+      utl::getAttribute(child,"kappa",kappa);
       if (code == 0)
         prob.setMaterial(kappa);
       else if (setPropertyType(code,Property::MATERIAL,mVec.size()))
         mVec.push_back(kappa);
     }
+
     else if (!strcasecmp(child->Value(),"source")) {
-      if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"line")) {
-        double L=0;
-        if (child->Attribute("L"))
-          L = atof(child->Attribute("L"));
+      std::string type;
+      utl::getAttribute(child,"type",type,true);
+      if (type == "line") {
+        double L = 0.0;
+        utl::getAttribute(child,"L",L);
         std::cout <<"\nHeat source function: Line L="<< L << std::endl;
         prob.setSource(new PoissonLineSource(L));
-      } else if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"expression")) {
-        if (child->FirstChild() && child->FirstChild()->Value()) {
-          std::cout << "\nHeat source function: " 
-                    << child->FirstChild()->Value() << std::endl;
-          prob.setSource(new EvalFunction(child->FirstChild()->Value()));
-        }
-      } else
-        std::cerr <<"  ** SIMPoisson1D::parse: Unknown source function "
-                  << (child->Attribute("type")?child->Attribute("type"):"") << std::endl;
-    } else if (!strcasecmp(child->Value(),"anasol")) {
-      if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"line")) {
-        double L=0;
-        if (child->Attribute("L"))
-          L = atof(child->Attribute("L"));
+      }
+      else if (type == "expression" && child->FirstChild()) {
+        std::cout <<"\nHeat source function: "
+                  << child->FirstChild()->Value() << std::endl;
+        prob.setSource(new EvalFunction(child->FirstChild()->Value()));
+      }
+      else
+        std::cerr <<"  ** SIMPoisson1D::parse: Invalid source function "
+                  << type << std::endl;
+    }
+
+    else if (!strcasecmp(child->Value(),"anasol")) {
+      int code = 0;
+      std::string type;
+      utl::getAttribute(child,"type",type,true);
+      if (type == "line") {
+        double L = 0.0;
+        utl::getAttribute(child,"L",L);
         std::cout <<"\nAnalytical solution: Line L="<< L << std::endl;
         mySol = new AnaSol(NULL,new PoissonLine(L));
-      } else if (child->Attribute("type") &&
-                 !strcasecmp(child->Attribute("type"),"expression")) {
-        std::string variables, primary, secondary;
-        const TiXmlElement* var = child->FirstChildElement("variables");
-        if (var && var->FirstChild() && var->FirstChild()->Value()) {
-          variables = var->FirstChild()->Value();
-          if (variables[variables.size()-1] != ';')
-            variables += ";";
-        }
-        const TiXmlElement* prim = child->FirstChildElement("primary");
-        RealFunc* s = NULL;
-        VecFunc* v = NULL;
-        if (prim && prim->FirstChild() && prim->FirstChild()->Value()) {
-          primary = prim->FirstChild()->Value();
-          s = new EvalFunction((variables+primary).c_str());
-        }
-        const TiXmlElement* sec = child->FirstChildElement("secondary");
-        if (sec && sec->FirstChild() && sec->FirstChild()->Value()) {
-          secondary = sec->FirstChild()->Value();
-          v = new VecFuncExpr(secondary,variables);
-        }
-        std::cout <<"\nAnalytical solution:" << std::endl;
-        if (!variables.empty())
-          std::cout << "\t Variables=" << variables << std::endl;
-        if (s)
-          std::cout << "\t Primary=" << primary << std::endl;
-        if (v)
-          std::cout << "\t Secondary=" << secondary << std::endl;
-        mySol = new AnaSol(s, v);
-      } else
-        std::cerr <<"  ** SIMPoisson1D::parse: Unknown analytical solution "
-          << (child->Attribute("type")?child->Attribute("type"):"") << std::endl;
+      }
+      else if (type == "expression") {
+        std::cout <<"\nAnalytical solution: Expression"<< std::endl;
+        mySol = new AnaSol(child);
+      }
+      else
+        std::cerr <<"  ** SIMPoisson1D::parse: Invalid analytical solution "
+                  << type <<" (ignored)"<< std::endl;
 
-        // Define the analytical boundary traction field
-        size_t code=0;
-        if (child->Attribute("code"))
-          code = atoi(child->Attribute("code"));
-        if (code > 0 && mySol && mySol->getScalarSecSol())
+      // Define the analytical boundary traction field
+      if (utl::getAttribute(child,"code",code))
+	if (code > 0 && mySol && mySol->getScalarSecSol())
         {
           setPropertyType(code,Property::NEUMANN);
           myVectors[code] = mySol->getScalarSecSol();
           myAFcode = code;
         }
-      }
-      else
-        SIM1D::parse(child);
-
-      child = child->NextSiblingElement();
     }
+
+    child = child->NextSiblingElement();
+  }
 
   return true;
 }
--- SIMPoisson2D.C
+++ SIMPoisson2D.C
@@ -13,16 +13,15 @@
 
 #include "SIMPoisson2D.h"
 #include "PoissonSolutions.h"
+#include "Functions.h"
 #include "Utilities.h"
 #include "AnaSol.h"
+#include "tinyxml.h"
+
 #ifdef HAS_LRSPLINE
 #include "LR/ASMu2D.h"
 #include "Profiler.h"
 #endif
-#include <string.h>
-#include "Functions.h"
-
-#include "tinyxml.h"
 
 
 SIMPoisson2D::~SIMPoisson2D ()
@@ -108,50 +107,13 @@ bool SIMPoisson2D::parse (char* keyWord, std::istream& is)
 
   else if (!strncasecmp(keyWord,"ANASOL",6))
   {
+    int code = -1;
     cline = strtok(keyWord+6," ");
-    int code=-1;
     if (!strncasecmp(cline,"SQUARE",6))
     {
       double L = atof(strtok(NULL," "));
       std::cout <<"\nAnalytical solution: Square L="<< L << std::endl;
       mySol = new AnaSol(NULL,new Square2D(L));
-    }
-    else if (!strncasecmp(cline,"EXPRESSION",10))
-    {
-      std::string primary, secondary, variables;
-      int lines = atoi(strtok(NULL, " "));
-      char* c = strtok(NULL, " ");
-      if (c)
-        code = atoi(c);
-      else
-        code = 0;
-      RealFunc* s=NULL;
-      VecFunc* v=NULL;
-      for (int i = 0; i < lines; i++) {
-        std::string function = utl::readLine(is);
-        size_t pos;
-        if ((pos = function.find("Variables=")) != std::string::npos) {
-          variables += function.substr(pos+10);
-          if (variables[variables.size()-1] != ';')
-            variables += ";";
-        }
-        if ((pos = function.find("Primary=")) != std::string::npos) {
-          primary = function.substr(pos+8);
-          s = new EvalFunction((variables+primary).c_str());
-        }
-        if ((pos = function.find("Secondary=")) != std::string::npos) {
-          secondary = function.substr(pos+10);
-          v = new VecFuncExpr(secondary,variables);
-        }
-      }
-      std::cout <<"\nAnalytical solution:" << std::endl;
-      if (!variables.empty())
-        std::cout << "\t Variables=" << variables << std::endl;
-      if (s)
-        std::cout << "\t Primary=" << primary << std::endl;
-      if (v)
-        std::cout << "\t Secondary=" << secondary << std::endl;
-      mySol = new AnaSol(s, v);
     }
     else if (!strncasecmp(cline,"LSHAPE",6))
     {
@@ -174,21 +136,27 @@ bool SIMPoisson2D::parse (char* keyWord, std::istream& is)
       prob.setSource(new PoissonInteriorLayerSource());
 
       // Define the Dirichlet boundary condition from the analytical solution
-      size_t code = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
+      code = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
       if (code < 1)
       {
-        std::cerr <<"  ** SIMPoisson2D::parse: Specify code > 0 for the"
+        std::cerr <<" *** SIMPoisson2D::parse: Specify code > 0 for the"
 		  <<" inhomogenous DIRICHLET boundary on InteriorLayer\n";
         return false;
       }
-      for (size_t j = 0; j < myProps.size(); j++)
-        if (myProps[j].pindx == code && myProps[j].pcode == Property::UNDEFINED)
-          myProps[j].pcode = Property::DIRICHLET_INHOM;
+      this->setPropertyType(code,Property::DIRICHLET_INHOM);
       myScalars[code] = new PoissonInteriorLayerSol();
+      code = 0; // Avoid definition of Neumann property
+    }
+    else if (!strncasecmp(cline,"EXPRESSION",10))
+    {
+      std::cout <<"\nAnalytical solution: Expression"<< std::endl;
+      int lines = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
+      code = (cline = strtok(NULL," ")) ? atoi(cline) : 0;
+      mySol = new AnaSol(is,lines);
     }
     else
     {
-      std::cerr <<"  ** SIMPoisson2D::parse: Unknown analytical solution "
+      std::cerr <<"  ** SIMPoisson2D::parse: Invalid analytical solution "
 		<< cline <<" (ignored)"<< std::endl;
       return true;
     }
@@ -210,168 +178,109 @@ bool SIMPoisson2D::parse (char* keyWord, std::istream& is)
   return true;
 }
 
+
 bool SIMPoisson2D::parse (const TiXmlElement* elem)
 {
   if (strcasecmp(elem->Value(),"poisson"))
-    return SIM2D::parse(elem);
+    return this->SIM2D::parse(elem);
 
   std::vector<const TiXmlElement*> parsed = handlePriorityTags(elem);
+
   const TiXmlElement* child = elem->FirstChildElement();
   while (child) {
-    if (find(parsed.begin(),parsed.end(),child) != parsed.end()) {
-      child = child->NextSiblingElement();
-      continue;
-    }
-    if (!strcasecmp(child->Value(),"isotropic")) {
+    if (find(parsed.begin(),parsed.end(),child) != parsed.end())
+      ;
+
+    else if (!strcasecmp(child->Value(),"isotropic")) {
       int code = 0;
-      if (child->Attribute("code"))
-        code = atoi(child->Attribute("code"));
-      double kappa=1000.f;
-      if (child->Attribute("kappa"))
-        kappa = atof(child->Attribute("kappa"));
+      double kappa = 1000.0;
+      utl::getAttribute(child,"code",code);
+      utl::getAttribute(child,"kappa",kappa);
       if (code == 0)
         prob.setMaterial(kappa);
       else if (setPropertyType(code,Property::MATERIAL,mVec.size()))
         mVec.push_back(kappa);
     }
+
     else if (!strcasecmp(child->Value(),"source")) {
-      if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"square")) {
-        double L=0;
-        if (child->Attribute("L"))
-          L = atof(child->Attribute("L"));
+      std::string type;
+      utl::getAttribute(child,"type",type,true);
+      if (type == "square") {
+        double L = 0.0;
+        utl::getAttribute(child,"L",L);
         std::cout <<"\nHeat source function: Square L="<< L << std::endl;
         prob.setSource(new Square2DHeat(L));
-      } else if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"expression")) {
-        if (child->FirstChild() && child->FirstChild()->Value()) {
-          std::cout << "\nHeat source function: " 
-                    << child->FirstChild()->Value() << std::endl;
-          prob.setSource(new EvalFunction(child->FirstChild()->Value()));
-        }
-      } else
-        std::cerr <<"  ** SIMPoisson2D::parse: Unknown source function "
-                  << (child->Attribute("type")?child->Attribute("type"):"") << std::endl;
-    } else if (!strcasecmp(child->Value(),"anasol")) {
-      if (child->Attribute("type") &&
-          !strcasecmp(child->Attribute("type"),"square")) {
-        double L=0;
-        if (child->Attribute("L"))
-          L = atof(child->Attribute("L"));
+      }
+      else if (type == "expression" && child->FirstChild()) {
+        std::cout <<"\nHeat source function: "
+                  << child->FirstChild()->Value() << std::endl;
+        prob.setSource(new EvalFunction(child->FirstChild()->Value()));
+      }
+      else
+        std::cerr <<"  ** SIMPoisson2D::parse: Invalid source function "
+                  << type << std::endl;
+    }
+
+    else if (!strcasecmp(child->Value(),"anasol")) {
+      int code = 0;
+      std::string type;
+      utl::getAttribute(child,"type",type,true);
+      if (type == "square") {
+        double L = 0.0;
+        utl::getAttribute(child,"L",L);
         std::cout <<"\nAnalytical solution: Square L="<< L << std::endl;
         mySol = new AnaSol(NULL,new Square2D(L));
-      } else if (child->Attribute("type") &&
-                 !strcasecmp(child->Attribute("type"),"lshape")) {
+      }
+      else if (type == "lshape") {
         std::cout <<"\nAnalytical solution: Lshape"<< std::endl;
         mySol = new AnaSol(NULL,new LshapePoisson());
-      } else if (child->Attribute("type") &&
-                 !strcasecmp(child->Attribute("type"),"sinussquare")) {
+      }
+      else if (type == "sinussquare") {
         std::cout <<"\nAnalytical solution: SquareSinus"
                   <<"\nHeat source function: SquareSinusSource"<< std::endl;
         mySol = new AnaSol(NULL,new SquareSinus());
         prob.setSource(new SquareSinusSource());
-      } else if (child->Attribute("type") &&
-                 !strcasecmp(child->Attribute("type"),"interiorlayer")) {
+      }
+      else if (type == "interiorlayer") {
         std::cout <<"\nAnalytical solution: InteriorLayer"
-          <<"\nHeat source function: InteriorLayerSource"<< std::endl;
+                  <<"\nHeat source function: InteriorLayerSource"<< std::endl;
         mySol = new AnaSol(new PoissonInteriorLayerSol(),
-            new PoissonInteriorLayer());
+                           new PoissonInteriorLayer());
         prob.setSource(new PoissonInteriorLayerSource());
 
         // Define the Dirichlet boundary condition from the analytical solution
-        size_t code=0;
-        if (child->Attribute("code"))
-          code = atoi(child->Attribute("code"));
+        utl::getAttribute(child,"code",code);
         if (code < 1)
         {
-          std::cerr <<"  ** SIMPoisson2D::parse: Specify code > 0 for the"
-            <<" inhomogenous DIRICHLET boundary on InteriorLayer\n";
+          std::cerr <<" *** SIMPoisson2D::parse: Specify code > 0 for the"
+                    <<" inhomogenous DIRICHLET boundary on InteriorLayer\n";
           return false;
         }
-        for (size_t j = 0; j < myProps.size(); j++)
-          if (myProps[j].pindx == code && myProps[j].pcode == Property::UNDEFINED)
-            myProps[j].pcode = Property::DIRICHLET_INHOM;
+        this->setPropertyType(code,Property::DIRICHLET_INHOM);
         myScalars[code] = new PoissonInteriorLayerSol();
-      } else if (child->Attribute("type") &&
-                 !strcasecmp(child->Attribute("type"),"expression")) {
-        std::string variables, primary, secondary;
-        const TiXmlElement* var = child->FirstChildElement("variables");
-        if (var && var->FirstChild() && var->FirstChild()->Value()) {
-          variables = var->FirstChild()->Value();
-          if (variables[variables.size()-1] != ';')
-            variables += ";";
-        }
-        const TiXmlElement* prim = child->FirstChildElement("primary");
-        RealFunc* s = NULL;
-        VecFunc* v = NULL;
-        if (prim && prim->FirstChild() && prim->FirstChild()->Value()) {
-          primary = prim->FirstChild()->Value();
-          s = new EvalFunction((variables+primary).c_str());
-        }
-        const TiXmlElement* sec = child->FirstChildElement("secondary");
-        if (sec && sec->FirstChild() && sec->FirstChild()->Value()) {
-          secondary = sec->FirstChild()->Value();
-          v = new VecFuncExpr(secondary,variables);
-        }
-        std::cout <<"\nAnalytical solution:" << std::endl;
-        if (!variables.empty())
-          std::cout << "\t Variables=" << variables << std::endl;
-        if (s)
-          std::cout << "\t Primary=" << primary << std::endl;
-        if (v)
-          std::cout << "\t Secondary=" << secondary << std::endl;
-        mySol = new AnaSol(s, v);
-      } else
-        std::cerr <<"  ** SIMPoisson2D::parse: Unknown analytical solution "
-          << (child->Attribute("type")?child->Attribute("type"):"") << std::endl;
+      }
+      else if (type == "expression") {
+        std::cout <<"\nAnalytical solution: Expression"<< std::endl;
+        mySol = new AnaSol(child);
+      }
+      else
+        std::cerr <<"  ** SIMPoisson2D::parse: Invalid analytical solution "
+                  << type <<" (ignored)"<< std::endl;
 
-        // Define the analytical boundary traction field
-        size_t code=0;
-        if (child->Attribute("code"))
-          code = atoi(child->Attribute("code"));
+      // Define the analytical boundary traction field
+      if (code == 0 && utl::getAttribute(child,"code",code))
         if (code > 0 && mySol && mySol->getScalarSecSol())
         {
           setPropertyType(code,Property::NEUMANN);
           myVectors[code] = mySol->getScalarSecSol();
           myAFcode = code;
         }
-      }
-      else
-        SIM2D::parse(child);
-
-      child = child->NextSiblingElement();
     }
+
+    child = child->NextSiblingElement();
+  }
 
   return true;
-
-#if 0
-  // Test code, move to SIM2D maybe?)
-  else if (!strncasecmp(keyWord,"LRREFINE",8))
-  {
-    PROFILE("LR refinement");
-    cline = strtok(keyWord+8," ");
-    int nRef;
-    ASMu2D* patch = static_cast<ASMu2D*>(myModel.front());
-    if (!strncasecmp(cline,"UNIFORM",7))
-    {
-      nRef = atoi(strtok(NULL," "));
-      std::cout <<"\nLR refinement UNIFORM : "<< nRef << std::endl;
-      patch->uniformRefine(nRef);
-    }
-    else if(!strncasecmp(cline,"CORNER",6))
-    {
-      nRef = atoi(strtok(NULL," "));
-      std::cout <<"\nLR refinement CORNER : "<< nRef << std::endl;
-      patch->cornerRefine(nRef);
-    }
-    else if(!strncasecmp(cline,"DIAGONAL",8))
-    {
-      nRef = atoi(strtok(NULL," "));
-      std::cout <<"\nLR refinement DIAGONAL : "<< nRef << std::endl;
-      patch->diagonalRefine(nRef);
-    }
-  }
-#endif
 }
 
 
