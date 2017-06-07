b9214ecc88cb7e6155a3128e07e31c29934f9882
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-06-07 15:17:13 +0200

    SIMPoisson: make constructor explicit

--- SIMPoisson.h
+++ SIMPoisson.h
@@ -39,7 +39,7 @@ template<class Dim> class SIMPoisson : public SIMMultiPatchModelGen<Dim>
 public:
   //! \brief Default constructor.
   //! \param[in] checkRHS If \e true, ensure the model is in a right-hand system
-  SIMPoisson(bool checkRHS = false) :
+  explicit SIMPoisson(bool checkRHS = false) :
     SIMMultiPatchModelGen<Dim>(1,checkRHS), prob(Dim::dimension)
   {
     Dim::myProblem = &prob;
