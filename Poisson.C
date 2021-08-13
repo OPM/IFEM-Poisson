35178d05b78eb6ba9a9262dc97256030ce40ac07
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-08-13 14:24:29 +0200

    fixed: sprintf warning
    buffer is too small to fit a full size_t

--- Poisson.C
+++ Poisson.C
@@ -576,7 +576,7 @@ std::string PoissonNorm::getName (size_t i, size_t j, const char* prefix) const
     if (j > 5 && nx > 1)
     {
       j -= 5;
-      char comp[16];
+      char comp[32];
       if (!anasol)
         sprintf(comp,"a(u^h,w%zu)",j);
       else if (j%2)
